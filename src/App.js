38a7266baa3238ac493296c5ba20cc5eb1cb50a3
Joary13 <jolea13008@gmail.com>
2023-12-20 23:05:46 +0100

    currency challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+export default function App() {
+  const [value, setValue] = useState('');
+  const [cur1, setCur1] = useState('USD');
+  const [cur2, setCur2] = useState('USD');
+  const [result, setResult] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?amount=${value}&from=${cur1}&to=${cur2}`
+        );
+        const data = await response.json();
+        if (data.rates) {
+          const conv = data.rates[cur2];
+          setResult(conv.toFixed(2));
+        }
+        setIsLoading(false);
+        // console.log(data?.rates[cur1]);
+        // if (data?.rates[cur1]) setResult(data?.rates[cur1]);
+      }
+      if (cur1 === cur2) return setResult(value);
+      convert();
+    },
+    [value, cur1, cur2]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={value}
+        type='text'
+        onChange={(e) => {
+          const res = e.target.value;
+          if (res.match(/[1-9]+/)) setValue(Number(res));
+          else {
+            setValue('');
+          }
+        }}
+        disabled={isLoading}
+      />
+      <CurrencyChoice
+        key={Date.now()}
+        value={cur1}
+        onSetCurrency={setCur1}
+        isLoading={isLoading}
+      />
+      <CurrencyChoice
+        key={Date.now() + 1}
+        value={cur2}
+        onSetCurrency={setCur2}
+        isLoading={isLoading}
+      />
+      <p>{result ? `${result} ${cur2}` : 'enter an amount'}</p>
     </div>
   );
 }
 
-export default App;
+function CurrencyChoice({ value, onSetCurrency, isLoading }) {
+  return (
+    <select
+      value={value}
+      onChange={(e) => onSetCurrency(e.target.value)}
+      disabled={isLoading}
+    >
+      <option value='USD'>USD</option>
+      <option value='EUR'>EUR</option>
+      <option value='CAD'>CAD</option>
+      <option value='INR'>INR</option>
+    </select>
+  );
+}
